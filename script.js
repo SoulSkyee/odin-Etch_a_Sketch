add9f9abeb214550b7b0c3134f1b8e25dbe630ed
SoulSkyee <benjiminfrinklin@gmail.com>
2025-12-18 18:25:05 +0700

    add v1.0 hover by mouseover

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const containerDiv = document.querySelector(".container");
+const contentDiv = document.querySelector(".container");
 
-for (let index = 0; index < 16; index++) {
-    const square = document.createElement("div");
-    containerDiv.appendChild(square);
+for (let i = 0; i < 16 * 16; i++) {
+    const contentSquare = document.createElement("div");
+    contentSquare.classList.add("square")
+    contentSquare.addEventListener("mouseover", () => {
+        contentSquare.style.backgroundColor = "blue";
+    });
+    contentDiv.appendChild(contentSquare);
 }
