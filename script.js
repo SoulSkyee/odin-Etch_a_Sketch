0c3178443fc4ebab355770e9ef888e645b3003dc
SoulSkyee <benjiminfrinklin@gmail.com>
2025-12-19 11:15:46 +0700

    add gridSizing

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+const gridSize = 81;
+
 const contentDiv = document.querySelector(".container");
 
-for (let i = 0; i < 16 * 16; i++) {
+// cara 2 - efisien
+contentDiv.style.setProperty("--grid-rows",gridSize);
+
+for (let i = 0; i < gridSize * gridSize; i++) {
     const contentSquare = document.createElement("div");
+    // cara 1
+    // contentSquare.style.width = `calc(100% / ${gridSize})`;
+    // contentSquare.style.height = `calc(100% / ${gridSize})`;
     contentSquare.classList.add("square")
     contentSquare.addEventListener("mouseover", () => {
         contentSquare.style.backgroundColor = "blue";
